f9438d51c91a71707e89fbbae50b35e9887c34e7
MarkHsiu <markhsiu@gmail.com>
2015-10-20 20:30:09 +0800

    add design pattern

--- pattern-design/decorator.js
+++ pattern-design/decorator.js
@@ -2,7 +2,40 @@
  * Created by MarkHsiu on 2015/10/20.
  */
 
-var tree={};
-tree.decorate =function(){
-    console.log();
+var tree = { };
+tree.decorate = function ( ){
+    console.log('Make sure the tree won\'t fall');
 }
+
+tree.getDecorator = function (deco){
+    tree[deco].prototype = this;
+    return new tree[deco];
+};
+
+tree.RedBalls = function ( ){
+    this.decorate = function ( ){
+        this.RedBalls.prototype.decorate();
+        console.log('Put on some red balls');
+    }
+}
+
+tree.BlueBalls = function ( ){
+    this.decorate = function( ){
+        this.BlueBalls.prototype.decorate( );
+        console.log ('Add a blue balls');
+    }
+}
+
+tree.Angel = function ( ){
+    this.decorate = function ( ){
+        this.Angel.prototype.decorate();
+        console.log('The a angel girl ');
+    }
+}
+
+
+tree = tree.getDecorator('RedBalls');
+tree = tree.getDecorator('BlueBalls');
+tree = tree.getDecorator('Angel');
+tree.decorate();
+
---
+++ pattern-design/observer.js
@@ -0,0 +1,71 @@
+/**
+ * Created by MarkHsiu on 2015/10/20.
+ */
+
+var  observer = {
+    addSubscriber: function(callback){
+        this.subscribers[this.subscribers.length] = callback;
+    },
+    removeSubscriber: function(callback){
+        for(var i=0,len=this.subscribers.length; i<len; i++){
+            if(this.subscribers[i] === callback){
+                delete (this.subscribers[i]);
+            }
+        }
+    },
+    publish: function(what){
+        for(var i=0,len=this.subscribers.length; i<len; i++){
+            if(typeof this.subscribers[i] === 'function'){
+                this.subscribers[i](what);
+            }
+        }
+    },
+    make: function(o){   // turns an object into a publisher
+        for (var i in this) {
+            o[i] = this[i];
+            o.subscribers = [];
+        }
+    }
+};
+
+
+var blogger = {
+    writeBlogPost: function(){
+        var content = 'Today is '+ new Date();
+        this.publish(content);
+    }
+};
+
+var la_times = {
+    newIssue: function() {
+        var paper = 'Martians have landed on Earth';
+        this.publish(paper);
+    }
+}
+
+observer.make(blogger);
+observer.make(la_times);
+
+var jack = {
+    read: function(what) {
+        console.log('I just read that '+what);
+    }
+};
+
+var jill = {
+    gossip: function(what) {
+        console.log('You didn\'t hear it form me , but '+ what);
+    }
+};
+
+blogger.addSubscriber(jack.read);
+blogger.addSubscriber(jill.gossip);
+console.log('----------------------------------');
+blogger.writeBlogPost();
+console.log('----------------------------------');
+blogger.removeSubscriber(jill.gossip);
+blogger.writeBlogPost();
+console.log('----------------------------------');
+la_times.addSubscriber(jill.gossip);
+console.log('----------------------------------');
+la_times.newIssue();
