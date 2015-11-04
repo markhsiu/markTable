561d9672253c9b28b70a91479c96b50e494ec449
MarkHsiu <markhsiu@gmail.com>
2015-11-04 23:47:51 +0800

    add design pattern

--- pattern-design/decorator.js
+++ pattern-design/decorator.js
@@ -5,7 +5,7 @@
 var tree = {};
 tree.decorate = function(){
     console.log('Make sure the tree won\'t fall');
-}
+};
 
 tree.getDecorator = function(deco){
     tree[deco].prototype = this;
@@ -16,22 +16,22 @@ tree.RedBalls = function(){
     this.decorate = function(){
         this.RedBalls.prototype.decorate();
         console.log('Put on some red balls');
-    }
-}
+    };
+};
 
 tree.BlueBalls = function(){
     this.decorate = function(){
         this.BlueBalls.prototype.decorate();
         console.log('Add a blue balls');
-    }
-}
+    };
+};
 
 tree.Angel = function(){
     this.decorate = function(){
         this.Angel.prototype.decorate();
         console.log('The a angel girl ');
-    }
-}
+    };
+};
 
 
 tree = tree.getDecorator('RedBalls');
